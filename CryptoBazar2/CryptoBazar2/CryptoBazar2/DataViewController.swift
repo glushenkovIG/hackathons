064e4686ab513a7a1417d392008c8d4a4bd56faa
bibloman <glushenkov.ig@phystech.edu>
2018-10-30 10:13:52 +0300

    hhh

---
+++ CryptoBazar2/CryptoBazar2/CryptoBazar2/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  CryptoBazar2
+//
+//  Created by Ivan Glushenkov on 19/10/2018.
+//  Copyright © 2018 Ivan Glushenkov. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
